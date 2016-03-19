7fb698558eb29a8903751e31efe03876159c6497
Alex Nichol <unixpickle@gmail.com>
2016-03-19 15:25:42 -0400

    drafted executable parsing

--- executable.go
+++ executable.go
@@ -1,6 +1,9 @@
 package mips32
 
-import "errors"
+import (
+	"errors"
+	"strconv"
+)
 
 type Executable struct {
 	// Segments maps chunks of instructions to various parts of the address space.
@@ -8,6 +11,70 @@ type Executable struct {
 
 	// Symbols maps symbol names to their addresses.
 	Symbols map[string]uint32
+}
+
+// ParseExecutable turns a tokenized source file into an executable blob.
+//
+// If the executable cannot be parsed for any reason, this will fail.
+// Overlapping .text sections, invalid instructions, and repeated symbols will all cause errors.
+func ParseExecutable(lines []TokenizedLine) (*Executable, error) {
+	var segmentStart uint32
+	var instructionAddr uint32
+	res := &Executable{
+		Segments: map[uint32][]Instruction{},
+		Symbols:  map[string]uint32{},
+	}
+	for _, line := range lines {
+		if line.Instruction != nil {
+			parsed, err := ParseTokenizedInstruction(line.Instruction)
+			if err != nil {
+				return nil, errors.New("line " + strconv.Itoa(line.LineNumber) + ": " + err.Error())
+			}
+			if res.addressInUse(instructionAddr) {
+				return nil, addressInUseError(line.LineNumber, instructionAddr)
+			}
+			res.Segments[segmentStart] = append(res.Segments[segmentStart], *parsed)
+			instructionAddr += 4
+		} else if line.Directive != nil {
+			dir := line.Directive
+			if dir.Name == ".word" {
+				if res.addressInUse(instructionAddr) {
+					return nil, addressInUseError(line.LineNumber, instructionAddr)
+				}
+				nextInst := DecodeInstruction(dir.Constant)
+				res.Segments[segmentStart] = append(res.Segments[segmentStart], *nextInst)
+				instructionAddr += 4
+			} else if dir.Name == ".text" {
+				if dir.Constant&3 != 0 {
+					return nil, errors.New("line " + strconv.Itoa(line.LineNumber) +
+						": misaligned segment")
+				}
+				segmentStart = dir.Constant
+				instructionAddr = dir.Constant
+			} else {
+				return nil, errors.New("line " + strconv.Itoa(line.LineNumber) +
+					": unknown directive: " + dir.Name)
+			}
+		} else if line.SymbolMarker != nil {
+			sym := *line.SymbolMarker
+			if _, ok := res.Symbols[sym]; ok {
+				return nil, errors.New("line " + strconv.Itoa(line.LineNumber) +
+					": repeated symbol declaration: " + sym)
+			}
+			res.Symbols[sym] = instructionAddr
+		}
+	}
+	return res, nil
+}
+
+// addressInUse reports of a word-aligned address is being used by one of the segments.
+func (e *Executable) addressInUse(addr uint32) bool {
+	for segment, insts := range e.Segments {
+		if segment <= addr && segment+uint32(len(insts)*4) > addr {
+			return true
+		}
+	}
+	return false
 }
 
 // Instruction stores all of the information about an instruction in distinct fields.
@@ -72,4 +139,9 @@ func ParseTokenizedInstruction(t *TokenizedInstruction) (*Instruction, error) {
 	} else {
 		return nil, errors.New("unknown instruction: " + t.Name)
 	}
+}
+
+func addressInUseError(line int, addr uint32) error {
+	hexStr := "0x" + strconv.FormatUint(uint64(addr), 16)
+	return errors.New("line " + strconv.Itoa(line) + ": overwriting address " + hexStr)
 }
