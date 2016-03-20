dcf4c820a997aea50aad2078753e4de1217ea3d7
Alex Nichol <unixpickle@gmail.com>
2016-03-20 10:28:29 -0400

    jumps pass tests

--- emulator_test.go
+++ emulator_test.go
@@ -68,3 +68,71 @@ func TestEmulatorRegModifiers(t *testing.T) {
 		}
 	}
 }
+
+func TestEmulatorJumps(t *testing.T) {
+	code := `
+		# Seed the program with two random numbers.
+		LUI $1, 0xca6d
+		ORI $1, $1, 0x8c46       # $r1 = 0xca6d8c46
+		LUI $2, 0x0a93
+		ORI $2, $2, 0xd70b       # $r2 = 0x0a93d70b
+
+		J SYM1
+		NOP
+		ADDU $3, $1, $2
+
+		SYM1:
+		JAL SYM2                 # $r31 = 36
+		XOR $4, $1, $2           # $r4 = 0xC0FE5B4D
+		NOR $5, $1, $2
+
+		SYM2:
+		ORI $6, $0, 56           # $r6 = 56
+		JR $6
+		AND $7, $1, $2           # $r7 = 0xA018402
+		OR $8, $1, $2
+
+		SYM3: # 56
+		ORI $9, $0, 72           # $r9 = 72
+		JALR $10, $9             # $r10 = 68
+		AND $11, $1, $2          # $r11 = 0xA018402
+		OR $12, $1, $2
+
+		SYM4: # 72
+		LUI $13, 0xDEAD
+		ORI $13, $13, 0xBEEC     # $r13 = 0xDEADBEEC
+		JR $13
+		SLLV $14, $1, $2         # $r14 = 0x6c623000
+		SRLV $15, $1, $2
+
+		.text 0xDEADBEE8
+		LUI $16, 0x1337
+		SYM5: # 0xDEADBEEC
+		LUI $17, 1337            # $r17 = 0x05390000
+	`
+	lines, err := TokenizeSource(code)
+	if err != nil {
+		t.Fatal(err)
+	}
+	program, err := ParseExecutable(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+	memory := NewLazyMemory()
+	emulator := &Emulator{
+		Memory:     memory,
+		Executable: program,
+	}
+	for !emulator.Done() {
+		if err := emulator.Step(); err != nil {
+			t.Fatal(err)
+		}
+	}
+	regFile := RegisterFile{1: 0xca6d8c46, 2: 0x0a93d70b, 4: 0xC0FE5B4D, 6: 56, 7: 0xA018402, 9: 72,
+		10: 68, 11: 0xA018402, 13: 0xDEADBEEC, 14: 0x6c623000, 17: 0x05390000, 31: 36}
+	for i := 0; i < 32; i++ {
+		if regFile[i] != emulator.RegisterFile[i] {
+			t.Error("bad register", i, "-", emulator.RegisterFile[i])
+		}
+	}
+}
